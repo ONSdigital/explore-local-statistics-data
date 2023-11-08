f8a3469b8affe61fd07946c8e0205482b780e13c
sazzadur.rahman@ons.gov.uk <sazzadur.rahman@ons.gov.uk>
2023-11-08 12:41:25 +0000

    test script child-obesity

---
+++ child-obesity/wrangling.py
@@ -0,0 +1,31 @@
+    
+"Aged 4 to 5 years underweight",
+             
+               
+"Aged 4 to 5 years healthy weight",
+                
+"Aged 4 to 5 years overweight",
+             
+"Aged 4 to 5 years obese",
+           
+ 
+"Aged 10 to 11 years underweight",
+
+
+"Aged 10 to 11 years healthy weight",
+
+                
+"Aged 10 to 11 years overweight",
+                 
+               
+"Aged 10 to 11 years obese",
+             
+            
+'Reception underweight', 
+'Reception healthy weight',
+'Reception overweight',
+'Reception obesity (including severe obesity)',
+'Year 6 underweight', 
+'Year 6 healthy weight', 
+'Year 6 overweight',
+'Year 6 obesity (including severe obesity)'
