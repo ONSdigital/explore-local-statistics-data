312587e3f295e3a10be74cb3456c7481030c30c8
Charlee Humphries_ONS <123644243+Charlee-H@users.noreply.github.com>
2024-02-13 18:51:13 +0000

    Apprenticeship (#101)
    * Create apprenticeship folder
    
    * Add raw csv file
    
    * Delete apprenticeship/apprenticeship.md
    
    * Upload csv-w
    
    * JSON apprenticeship added
    
    * Upload csv-w file
    
    * Updated metadata
    
    * Fixed gregorian type in CSV
    
    * fixed csv
    
    * JSON fixed
    
    * Fixed issue in data
    
    Isles of Scilly had missing data - now fixed
    
    * Fixed geography definition and measure name
    
    * JSON fixed
    
    ---------
    
    Co-authored-by: Sazzadur Rahman <110844828+sazzadur-ons@users.noreply.github.com>
    Co-authored-by: Sazzadur Rahman <sazzadur.rahman@ons.gov.uk>

--- apprenticeship/wrangling.py
+++ apprenticeship/wrangling.py
@@ -13,6 +13,7 @@ def wrangle(input: Path(), output: Path()) -> None: # type: ignore
     #df['observation_status'] = df.apply(lambda x: 'x' if 'na' in str(x['observation']) else x['observation_status'], axis = 1)
 
     #Transform from academic year to Gregorian date 
+
     df['period'] = df.apply(lambda x: str(x['period'])[:4] + '-09-01T00:00:00/P1Y', axis = 1)
 
     df.to_csv(output, index=False)
